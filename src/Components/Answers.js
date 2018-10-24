2e4b9c161e6ff4cbfafa97f6445fb5a3eeedd247
Kiran <kirankrishann@gmail.com>
2018-10-24 18:18:41 +0530

    refactoring

--- src/Components/Answers.js
+++ src/Components/Answers.js
@@ -1,38 +1,15 @@
 import React from 'react';
-import questionnaire from "../Data";
 
-class Answers extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value: 'Select One'
-        }
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(e){
-        this.setState({
-            value: e.target.value  
-        })
-      }
-        render(){
-        let reformattedArray = questionnaire.map(function(obj){
-                    return obj.answerOptions;
-        });
-        //console.log(reformattedArray);
-        let finalArr = reformattedArray.reduce(function(pres,curr){
-                return pres.concat(curr);
-            })
-            //console.log(finalArr);
-        return (
-            <select value={this.props.value} onChange={this.handleChange} className="form-control" required>
-            <option value="">Select one</option>
-            { finalArr.map(function(finalres){
-                return <option key={finalres.id}>{finalres}</option>
-            })}
-            </select>          
+const Answers = (props) => {
+         return (
+            <select className="form-control" required>
+                <option hidden value="">Choose Here</option>
+                     { 
+                         props.options.map( (answer,i) =>
+                         <option key={i} value={answer}>{answer}</option>
+                     )}
+                </select>          
         );  
     }
-}
 
 export default Answers;
